6fc4d64e6cd5e38e725804cb85ecc92ff6c576e5
Monir <akhandbuet@gmail.com>
2022-04-28 01:43:20 +0200

    Signup page has created

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,58 @@
+// NPM packages
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Project files
+import InputField from "../components/InputField";
+import form from "../data/signUpForm.json";
+import { createUser } from "../scripts/firebaseAuth";
+import { createDocumentWithId } from "../scripts/fireStore";
+
+export default function SignUp({ uidState }) {
+  const [uid, setUID] = uidState;
+  const navigation = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [city, setCity] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onSignUp(event) {
+    event.preventDefault();
+
+    // 1 Create UID
+    const newUID = await createUser(email, password);
+
+    // 2 Create user document
+    const newUser = {
+      name: name,
+      age: age,
+      city: city,
+    };
+    const payload = await createDocumentWithId("users", newUID, newUser);
+
+    // If it works i will navigate to the dashboard page
+    // Else i will show an error message
+    if (payload.error) alert("Could not create user");
+    else {
+      setUID(newUID);
+      navigation("/dashboard");
+    }
+  }
+
+  return (
+    <div id="sign-up">
+      <h1>Create an account</h1>
+      <p>Create an account with us to be able to add and edit drivers.</p>
+      <form onSubmit={onSignUp}>
+        <InputField setup={form.name} state={[name, setName]} />
+        <InputField setup={form.email} state={[email, setEmail]} />
+        <InputField setup={form.age} state={[age, setAge]} />
+        <InputField setup={form.city} state={[city, setCity]} />
+        <InputField setup={form.password} state={[password, setPassword]} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
