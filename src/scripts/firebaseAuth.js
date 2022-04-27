5c130427ff24322217ac803bcff4961ad181cb95
Monir <akhandbuet@gmail.com>
2022-04-28 01:36:15 +0200

    Auth has intialized

---
+++ src/scripts/firebaseAuth.js
@@ -0,0 +1,29 @@
+// NPM Packages
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "firebase/auth";
+  
+  // Project file
+  import { authentification } from "./firebase";
+  
+  // Methods
+  export async function createUser(email, password) {
+    const userCredential = await createUserWithEmailAndPassword(
+      authentification,
+      email,
+      password
+    );
+  
+    return userCredential.user.uid;
+  }
+  
+  export async function loginUser(email, password) {
+    const userCredential = await signInWithEmailAndPassword(
+      authentification,
+      email,
+      password
+    );
+  
+    return userCredential.user.uid;
+  }
